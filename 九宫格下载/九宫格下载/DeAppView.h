865fc6ec77347d16526513075161bbc8fcf48828
desperado <foreverxiongtao@gmail.com>
2017-06-19 15:04:14 +0800

    create xib

---
+++ 九宫格下载/九宫格下载/DeAppView.h
@@ -0,0 +1,13 @@
+//
+//  DeAppView.h
+//  九宫格下载
+//
+//  Created by tao xiong on 17/6/19.
+//  Copyright © 2017年 xiongtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeAppView : UIView
+
+@end
---
+++ 九宫格下载/九宫格下载/DeAppView.m
@@ -0,0 +1,21 @@
+//
+//  DeAppView.m
+//  九宫格下载
+//
+//  Created by tao xiong on 17/6/19.
+//  Copyright © 2017年 xiongtao. All rights reserved.
+//
+
+#import "DeAppView.h"
+
+@implementation DeAppView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
