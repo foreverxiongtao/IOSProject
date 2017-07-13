3a23e0be17a11f6b80d93ef4656a0ecdd950f8e8
xiongtao <xiongtao@gmail.com>
2017-07-13 00:01:23 -0700

    完成喜马拉雅电台

--- 电台项目/电台项目/ViewController.m
+++ 电台项目/电台项目/ViewController.m
@@ -11,7 +11,9 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *title_bar_container;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
-
+@property (weak, nonatomic) IBOutlet UIImageView *iv_new_play_show;
+@property (weak, nonatomic) IBOutlet UIView *buttom_container_tab;
+@property(strong,nonatomic) UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -20,6 +22,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self initTitleBar];
+    [self fillScrollViewData];
+    [self initScrollView];
 }
 
 -(void)initTitleBar{
@@ -39,8 +43,28 @@
     
     [self.view bringSubviewToFront:self.title_bar_container];
     
+}
+
+#pragma mark --填充数据到scrollview--
+-(void)fillScrollViewData{
     
- 
+    self.imageView=[[UIImageView alloc]init];
+    [self.imageView setImage:[UIImage imageNamed:@"finditem_iwannabehere.png"]];
+    CGFloat x=CGRectGetMinX(self.iv_new_play_show.frame);
+    CGFloat Y=CGRectGetMaxY(self.iv_new_play_show.frame);
+    self.imageView.frame=CGRectMake(x, Y+50, 300, 75);
+    
+    [self.scrollview addSubview:self.imageView];
+    self.scrollview.contentInset=UIEdgeInsetsMake(50, 0, 50, 0);
+}
+
+#pragma mark --初始化scrollview--
+
+-(void)initScrollView{
+    CGFloat height=CGRectGetMaxY(self.imageView.frame);
+    self.scrollview.contentSize=CGSizeMake(0,height);
+    self.scrollview.showsVerticalScrollIndicator=NO;
+    [self.view bringSubviewToFront:self.buttom_container_tab];
     
 }
 
