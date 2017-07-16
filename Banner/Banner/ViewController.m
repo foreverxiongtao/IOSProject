50e2d5a35d41062254e4d81c2694429e019bd2b2
foreverxiongtao <xiongtao@jinlinbao.com>
2017-07-16 09:23:44 +0800

    轮播图完善

--- Banner/Banner/ViewController.m
+++ Banner/Banner/ViewController.m
@@ -14,6 +14,8 @@
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollview_banner;
 @property(assign,nonatomic)int currentPage;
 
+@property(strong,nonatomic) NSTimer *timer;
+
 @end
 
 @implementation ViewController
@@ -21,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initScrollViewData];
+    [self initTimer];
 }
 
 
@@ -56,6 +59,45 @@
     CGFloat offsetX=self.scrollview_banner.contentOffset.x;
     self.currentPage=(offsetX+100)/200;
     self.page_control.currentPage=self.currentPage;
+}
+
+-(void)nextImage{
+    
+    NSInteger page=self.page_control.currentPage;
+    if (page>=self.page_control.numberOfPages-1) {
+        page=0;
+    }
+    else{
+        page++;
+    }
+    CGFloat offset=page*self.scrollview_banner.frame.size.width;
+    //设置uiScrollview的偏移量
+    [self.scrollview_banner setContentOffset:CGPointMake(offset, 0) animated:YES];
+}
+
+-(void)initTimer{
+    self.timer=[NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(nextImage) userInfo:nil repeats:true];
+    
+    //nstimer 网络默认的线程的优先级就低，
+    
+    //获取当前的消息循环对象
+    NSRunLoop *runloop=[NSRunLoop currentRunLoop];
+    
+    //将当前的nstimer添加到消息循环对象中,提高nstimer的线程优先级
+    [runloop addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+
+-(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    //在拖拽之前清空计时器
+    [self.timer invalidate];
+    self.timer=nil;
+
+}
+
+-(void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset{
+    self.timer=[NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(nextImage) userInfo:nil repeats:YES];
+
 }
 
 @end
